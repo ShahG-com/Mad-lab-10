e64629a20ed687ccbc54dc8123890325fb62593d
ShahG-com <shahzaibadil03@gmail.com>
2025-11-03 12:41:57 +0500

    Lab-task-10

---
+++ assigment2/lib/screens/screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+          ],
+        ),
+      ),
+    );
+  }
+}
